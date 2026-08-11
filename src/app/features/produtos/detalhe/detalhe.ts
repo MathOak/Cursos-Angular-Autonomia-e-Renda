87da3e2c5481dcb722dec0816eaff739db4c3987
dev.Math <45424550+MathOak@users.noreply.github.com>
2026-08-11 13:18:55 -0300

    Aula 2: Flow e pipes

---
+++ src/app/features/produtos/detalhe/detalhe.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe',
+  imports: [],
+  templateUrl: './detalhe.html',
+  styleUrl: './detalhe.css',
+})
+export class Detalhe {}
