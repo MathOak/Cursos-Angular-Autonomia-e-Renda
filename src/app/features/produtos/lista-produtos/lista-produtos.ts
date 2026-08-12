8e8e36497cf092df885fe2b18c1f96e22703fb1c
dev.Math <45424550+MathOak@users.noreply.github.com>
2026-08-12 17:25:03 -0300

    Feat: Implementando Signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Produto } from '../produto/produto';
 
 @Component({
@@ -8,15 +8,54 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-    produtos = [
-    {nome: "notebook", preco:3500},
-    {nome: "mouse", preco: 150},
-    // {nome:"teclado", preco: 250.55}
-  ]
-  
+  produtos = signal<
+    {
+      nome: string;
+      preco: number;
+    }[]
+  >([]);
+  produtosNovos = [
+    { nome: 'notebook', preco: 3500 },
+    { nome: 'mouse', preco: 150 },
+    { nome: 'teclado', preco: 250.55 },
+  ];
 
-  exibirProduto(nome:string){
-    this.produtos.push({nome:"teclado", preco: 250.55})
-    console.log('Produto selecionado é '+ nome)
+  filtrarNovoProduto() {
+    /* Esta função irá filtar a lista atual de produtos 
+    e irá retornar um objeto novo que 
+    não esteja atualmente na lista de produtos */
+
+    /* Caso a lista de produtos não tenha item nenhum 
+    ele retorna e adiciona o primeiro item da lista de produtosNovos */
+    if (this.produtos().length === 0) return this.produtosNovos[0];
+
+    /* Verifica o tamanho da lista na tela, com o tamanho da lista de novos produtos
+    caso a lista de novos produtos seja maior ou igual, ele continua adicionando na tela
+    caso a lista de novos produtos seja menor, ele não faz nada
+    */
+    if (this.produtosNovos.length >= this.produtos().length) {
+      /* Returna o item na posição atual baseada na quantidade de itens na tela
+      Se tiver 2 item na tela, ele vai pegar o terceiro item na lista de novos produtos
+      */
+      return this.produtosNovos[this.produtos().length];
+    }
+    /* Caso nenhuma das condições anteriores sejam aplicadas, 
+    ele retorna um valor nulo para verificação na inclusão da lista */
+    return null;
+  }
+
+  adicionarProduto() {
+    let novoproduto: { nome: string; preco: number } | null = this.filtrarNovoProduto();
+
+    /* Caso a minha função retorne um item novo, eu adiciono na lista */
+    if (novoproduto) {
+      this.produtos.update((listaAtual) => [...listaAtual, novoproduto]);
+    } else {
+      /* Caso contrario, não faço nada */
+    }
+  }
+
+  exibirProduto(nome: string) {
+    console.log('Produto selecionado é ' + nome);
   }
 }
